e23eb6c15be10e29f976a321e7d117638e9f5583
J W <woodjon@gmail.com>
2018-06-26 20:33:29 -0400

    Add Custom Vision example

---
+++ Cognitive Services/Custom Vision/HerbPrediction/HerbPrediction/Keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerbPrediction
+{
+    public class Keys
+    {
+        public string PredictionKey { get; set; }
+        public string TrainingKey { get; set; }
+    }
+}
---
+++ Cognitive Services/Custom Vision/HerbPrediction/HerbPrediction/Program.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Cognitive.CustomVision.Prediction;
+using Microsoft.Cognitive.CustomVision.Training;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HerbPrediction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Keys keys = null;
+
+            using (var reader = new StreamReader("App.json"))
+            {
+                var json = reader.ReadToEnd();
+
+                keys = JsonConvert.DeserializeObject<Keys>(json);
+            }
+
+            var trainingApi = new TrainingApi { ApiKey = keys.TrainingKey };
+            var predictionEndpoint = new PredictionEndpoint { ApiKey = keys.PredictionKey };
+
+            var projects = trainingApi.GetProjects();
+            var herbProject = projects.Where(p => p.Name == "Herbs").FirstOrDefault();
+
+            Console.WriteLine("Predicting basil image");
+            var imageFile = File.OpenRead("basil_test.jpg");
+
+            var result = predictionEndpoint.PredictImage(herbProject.Id, imageFile);
+            
+            foreach(var prediction in result.Predictions)
+            {
+                Console.WriteLine($"Tag: {prediction.Tag} Probability: {prediction.Probability}");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
