3a00e1699c764558411c714d60b4c315c60b2c98
J W <woodjon@gmail.com>
2018-07-17 10:37:48 -0400

    Add small project to show how to load the model

---
+++ ML.NET/MLNetExamples/LoadModel/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.ML;
+using MLNetShared;
+
+namespace LoadModel
+{
+    class Program
+    {
+        private static int YEARS_EXPERIENCE = 8;
+
+        static void Main(string[] args)
+        {
+            var modelPath = MLNetUtilities.GetModelFilePath("model.zip");
+
+            var model = PredictionModel.ReadAsync<SalaryData, SalaryPrediction>(modelPath).Result;
+
+            var prediction = model.Predict(new SalaryData { YearsExperience = YEARS_EXPERIENCE });
+
+            Console.WriteLine($"Prediction for {YEARS_EXPERIENCE} years - {prediction.PredictedSalary}");
+
+            // Can write it back to the file system
+            // model.WriteAsync("model.zip");
+
+            Console.ReadLine();
+        }
+    }
+}
